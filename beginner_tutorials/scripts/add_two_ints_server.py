057d2a35ef170b26a7a65486a22e3a5ecdf418ab
Madhur Behl <madhur.behl@virginia.edu>
2023-09-14 19:06:44 -0400

    Update add_two_ints_server.py

--- beginner_tutorials/scripts/add_two_ints_server.py
+++ beginner_tutorials/scripts/add_two_ints_server.py
@@ -1,17 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import roslib; roslib.load_manifest('beginner_tutorials')
 
 from beginner_tutorials.srv import *
 import rospy
 
 def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    print("Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b)))
     return AddTwoIntsResponse(req.a + req.b)
 
 def add_two_ints_server():
     rospy.init_node('add_two_ints_server')
     s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
-    print "Ready to add two ints."
+    print("Ready to add two ints.")
     rospy.spin()
 
 if __name__ == "__main__":
