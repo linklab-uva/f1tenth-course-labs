03e4808f81a15ea602c83914079cd73644a37a70
Madhur Behl <madhur.behl@virginia.edu>
2023-09-14 19:06:15 -0400

    Update add_two_ints_client.py

--- beginner_tutorials/scripts/add_two_ints_client.py
+++ beginner_tutorials/scripts/add_two_ints_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import roslib; roslib.load_manifest('beginner_tutorials')
 
 import sys
@@ -12,8 +12,8 @@ def add_two_ints_client(x, y):
         add_two_ints = rospy.ServiceProxy('add_two_ints', AddTwoInts)
         resp1 = add_two_ints(x, y)
         return resp1.sum
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+    except rospy.ServiceException as e:
+        print("Service call failed: %s"%e)
 
 def usage():
     return "%s [x y]"%sys.argv[0]
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         x = int(sys.argv[1])
         y = int(sys.argv[2])
     else:
-        print usage()
+        print(usage())
         sys.exit(1)
-    print "Requesting %s+%s"%(x, y)
-    print "%s + %s = %s"%(x, y, add_two_ints_client(x, y))
+    print("Requesting %s+%s"%(x, y))
+    print("%s + %s = %s"%(x, y, add_two_ints_client(x, y)))
